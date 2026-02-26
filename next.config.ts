e9c484b2d3d281ae474bc9dff037b052512afb51
asad-tech-dev <sidtechdevpk@gmail.com>
2026-02-26 17:04:28 +0500

    Revert "Added statis export in next.config"
    This reverts commit edf4252a9f8ad596c1fca7882433f4e2924a3c77.

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   // Uncomment for static hosting:
-  output: 'export',
+  // output: 'export',
 };
 
 export default nextConfig;
